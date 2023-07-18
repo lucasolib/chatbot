7ee32ebde2e2bf79463704c7ea548e597402a58e
Lucas de Oliveira Batista <lucasolibatista@gmail.com>
2023-07-17 21:33:47 -0300

    build: Starting the project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Optus</h1>
+      <h4>419k people like this including Maha Mourad and 35 Friends</h4>
+      <h1>"CHAT"</h1>
+    </>
+  )
+}
+
+export default App
